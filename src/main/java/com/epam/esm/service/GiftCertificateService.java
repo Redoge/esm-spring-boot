ed16602215dac6df5363a8b1e045328ed26ad8e1
Redoge <mail.redoge@gmail.com>
2023-03-14 01:11:00 +0200

    added services

---
+++ src/main/java/com/epam/esm/service/GiftCertificateService.java
@@ -0,0 +1,94 @@
+package com.epam.esm.service;
+
+import com.epam.esm.entity.GiftCertificate;
+import com.epam.esm.entity.Tag;
+import com.epam.esm.pojo.GiftCertificateSaveRequestPojo;
+import com.epam.esm.pojo.GiftCertificateSearchRequestPojo;
+import com.epam.esm.repository.GiftCertificateRepository;
+import com.epam.esm.util.mapper.GiftCertificateMapper;
+import com.epam.esm.util.sorter.GiftCertificateSorter;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static io.micrometer.common.util.StringUtils.isNotEmpty;
+
+@Service
+public class GiftCertificateService {
+    private final GiftCertificateRepository giftCertificateDao;
+    private final GiftCertificateSorter giftCertificateSorter;
+    private final GiftCertificateMapper giftCertificateMapper;
+    private final TagService tagService;
+
+    public GiftCertificateService(GiftCertificateRepository giftCertificateDao, GiftCertificateSorter giftCertificateSorter,
+                                  GiftCertificateMapper giftCertificateMapper, TagService tagService) {
+        this.giftCertificateDao = giftCertificateDao;
+        this.giftCertificateSorter = giftCertificateSorter;
+        this.giftCertificateMapper = giftCertificateMapper;
+        this.tagService = tagService;
+
+    }
+
+    public List<GiftCertificate> getAll() {
+        return giftCertificateDao.findAll();
+    }
+
+    public Optional<GiftCertificate> getById(long id) {
+        return giftCertificateDao.findById(id);
+    }
+
+    public Optional<GiftCertificate> getByName(String name) {
+        return giftCertificateDao.findByName(name);
+    }
+
+    public void deleteById(long id) {
+        giftCertificateDao.deleteById(id);
+    }
+
+    public void save(GiftCertificate giftCertificate) {
+        giftCertificateDao.save(giftCertificate);
+    }
+
+    @Transactional
+    public void update(GiftCertificateSaveRequestPojo giftCertificatePojo) {
+        Optional<GiftCertificate> gCert = getById(giftCertificatePojo.getId());
+        if(gCert.isPresent()) {
+            var newGCert = giftCertificateMapper.createUpdatedGCertBySaveRequestPojoAndGCert(giftCertificatePojo, gCert.get());
+            List<Tag> tags = tagService.saveAll(giftCertificatePojo.getTags()
+                    .stream()
+                    .map(Tag::new)
+                    .toList());
+            newGCert.setTags(tags);
+            giftCertificateDao.save(newGCert);
+        }
+    }
+
+    public List<GiftCertificate> getByGiftCertificateSearchRequestPojo(GiftCertificateSearchRequestPojo certsSearchReqPojo) {
+        List<GiftCertificate> gCerts = getGiftCertificateMainDtoBySearchReq(certsSearchReqPojo);
+        return giftCertificateSorter.sortedGiftCertificateMainDtoBySearchReq(gCerts, certsSearchReqPojo);
+    }
+
+    private List<GiftCertificate> getGiftCertificateMainDtoBySearchReq(GiftCertificateSearchRequestPojo certsSearchReqPojo) {
+        List<GiftCertificate> gCerts;
+        var nameIsPresent = isNotEmpty(certsSearchReqPojo.getName());
+        var descriptionIsPresent = isNotEmpty(certsSearchReqPojo.getDescription());
+        var tagIsPresent = isNotEmpty(certsSearchReqPojo.getTagName());
+        if (nameIsPresent) {
+            gCerts = tagIsPresent ?
+                    giftCertificateDao.findByNameContainingAndTagsName(certsSearchReqPojo.getName(), certsSearchReqPojo.getTagName()) :
+                    giftCertificateDao.findByNameContaining(certsSearchReqPojo.getName());
+        } else if (descriptionIsPresent) {
+            gCerts = tagIsPresent ?
+                    giftCertificateDao.findByDescriptionContainingAndTagsName(certsSearchReqPojo.getDescription(), certsSearchReqPojo.getTagName()) :
+                    giftCertificateDao.findByDescriptionContaining(certsSearchReqPojo.getDescription());
+        } else {
+            gCerts = tagIsPresent ?
+                    giftCertificateDao.findByTagsName(certsSearchReqPojo.getTagName()) :
+                    getAll();
+        }
+        return gCerts;
+    }
+
+}
---
+++ src/main/java/com/epam/esm/service/TagService.java
@@ -0,0 +1,41 @@
+package com.epam.esm.service;
+
+import com.epam.esm.entity.Tag;
+import com.epam.esm.repository.TagRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TagService {
+    private final TagRepository tagDao;
+
+    public TagService(TagRepository tagDao) {
+        this.tagDao = tagDao;
+    }
+
+    public List<Tag> getAll() {
+        return tagDao.findAll();
+    }
+
+    public Optional<Tag> getById(long id) {
+        return tagDao.findById(id);
+    }
+
+    public Optional<Tag> getByName(String name) {
+        return tagDao.findByName(name);
+    }
+
+    public void deleteById(long id) {
+        tagDao.deleteById(id);
+    }
+
+    public void save(String tagName) {
+        tagDao.save(new Tag(tagName));
+    }
+    protected List<Tag> saveAll(List<Tag> tags){
+        return tagDao.saveAll(tags);
+    }
+}
