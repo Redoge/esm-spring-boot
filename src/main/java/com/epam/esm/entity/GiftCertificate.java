3050fad81df8e515cad25bb469cf0f72b413ef2f
Redoge <mail.redoge@gmail.com>
2023-03-14 01:10:00 +0200

    added entity

---
+++ src/main/java/com/epam/esm/entity/GiftCertificate.java
@@ -0,0 +1,48 @@
+package com.epam.esm.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GiftCertificate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gift_certificate_id")
+    private Long id;
+    @Column(name = "gift_certificate_name")
+    private String name;
+    @Column(name = "gift_certificate_description")
+    private String description;
+    @Column(name = "gift_certificate_price")
+    private BigDecimal price;
+    @Column(name = "gift_certificate_duration")
+    private int duration;
+    @Column(name = "gift_certificate_create_date")
+    private LocalDateTime createDate;
+    @Column(name = "gift_certificate_id_last_update_date")
+    private LocalDateTime lastUpdateDate;
+    @ManyToMany
+    @JoinTable(name = "gift_certificate_tag",
+            joinColumns = @JoinColumn(name = "gift_certificate_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<Tag> tags;
+
+    public GiftCertificate(Long id, String name, String description, BigDecimal price, int duration, LocalDateTime createDate, LocalDateTime lastUpdateDate) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.duration = duration;
+        this.createDate = createDate;
+        this.lastUpdateDate = lastUpdateDate;
+    }
+}
--- src/main/java/com/epam/esm/entity/Tag.java
+++ src/main/java/com/epam/esm/entity/Tag.java
@@ -1,2 +1,27 @@
-package com.epam.esm.entity;public class Tag {
+package com.epam.esm.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+    @Column(name = "tag_name")
+    private String name;
+    @ManyToMany(mappedBy = "tags")
+    private List<GiftCertificate> certificates;
+
+    public Tag(String name) {
+        this.name = name;
+    }
 }
