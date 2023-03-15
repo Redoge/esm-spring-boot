538ea0f1829d7b3e64eb0f4e580765c68b94d09e
Redoge <mail.redoge@gmail.com>
2023-03-15 23:04:07 +0200

    added @Component annotation

--- src/main/java/com/epam/esm/util/validators/GiftCertificateValidator.java
+++ src/main/java/com/epam/esm/util/validators/GiftCertificateValidator.java
@@ -1,10 +1,11 @@
 package com.epam.esm.util.validators;
 
 import com.epam.esm.entities.GiftCertificate;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 
 import static io.micrometer.common.util.StringUtils.isNotEmpty;
-
+@Component
 public class GiftCertificateValidator {
     public boolean isValid(GiftCertificate giftCertificate) {
         return isNotEmpty(giftCertificate.getName()) &&
