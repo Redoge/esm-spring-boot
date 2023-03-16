000429fd795644e91f98ad67358ed08a21deca0c
Redoge <mail.redoge@gmail.com>
2023-03-16 21:21:04 +0200

    added hateoas to TagController

--- src/main/java/com/epam/esm/controllers/TagController.java
+++ src/main/java/com/epam/esm/controllers/TagController.java
@@ -5,10 +5,17 @@ import com.epam.esm.exceptions.TagIsExistException;
 import com.epam.esm.exceptions.TagNotFoundException;
 import com.epam.esm.services.interfaces.TagServiceInterface;
 import jakarta.transaction.Transactional;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/api/tags")
@@ -19,14 +26,28 @@ public class TagController {
         this.tagService = tagService;
     }
     @GetMapping
-    public ResponseEntity<List<Tag>> getTags() {
-        return ResponseEntity.ok(tagService.getAll());
+    public CollectionModel<EntityModel<Tag>>getTags() throws TagNotFoundException, TagIsExistException {
+        List<Tag> tags = tagService.getAll();
+        List<EntityModel<Tag>> tagResources = new ArrayList<>();
+        for (var tag : tags) {
+            EntityModel<Tag> tagResource = EntityModel.of(tag);
+            tagResource.add(linkTo(methodOn(TagController.class).getTagById(tag.getId())).withSelfRel());
+            tagResource.add(linkTo(methodOn(TagController.class).removeTagById(tag.getId())).withRel("delete").withType(HttpMethod.DELETE.name()));
+            tagResources.add(tagResource);
+        }
+        CollectionModel<EntityModel<Tag>> resources = CollectionModel.of(tagResources);
+        resources.add(linkTo(methodOn(TagController.class).getTags()).withSelfRel());
+        resources.add(linkTo(methodOn(TagController.class).createTag(null)).withRel("create").withType(HttpMethod.POST.name()));
+        return resources;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getTagById(@PathVariable long id) throws TagNotFoundException {
+    public EntityModel<Tag> getTagById(@PathVariable long id) throws TagNotFoundException {
         var tag = tagService.getById(id);
-        return ResponseEntity.ok(tag.get());
+        EntityModel<Tag> tagResource = EntityModel.of(tag.get());
+        tagResource.add(linkTo(methodOn(TagController.class).getTagById(id)).withSelfRel());
+        tagResource.add(linkTo(methodOn(TagController.class).removeTagById(id)).withRel("delete").withType(HttpMethod.DELETE.name()));
+        return tagResource;
 
     }
 
