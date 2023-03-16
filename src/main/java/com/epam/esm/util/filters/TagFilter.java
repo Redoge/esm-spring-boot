b58b81eaf71d9bb40b9481b0ebb048a2ce67e7f2
Redoge <mail.redoge@gmail.com>
2023-03-16 19:46:57 +0200

    Fix gift certificates save method (you can create certs without tags)

--- src/main/java/com/epam/esm/util/filters/TagFilter.java
+++ src/main/java/com/epam/esm/util/filters/TagFilter.java
@@ -4,17 +4,24 @@ import com.epam.esm.entities.Tag;
 import com.epam.esm.services.interfaces.GiftCertificateServiceInterface;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
 
 @Component
 public class TagFilter {
     public List<Tag> isExistByName(List<Tag> tags, List<String> names) {
+        if(isEmpty(names))
+            return new ArrayList<>();
         return tags.stream()
-                .filter(tag -> names.contains(tag.getName()))
+                .filter(tag -> names.contains(tag.getName()))F
                 .toList();
     }
 
     public List<Tag> isNotExistByName(List<Tag> tags, List<String> names) {
+        if(isEmpty(names))
+            return new ArrayList<>();
         var existTagName = tags.stream()
                 .map(Tag::getName)
                 .toList();
