708c4c9d1d7253e102fee0e558fa4b52ab4085ec
Redoge <mail.redoge@gmail.com>
2023-04-30 22:21:51 +0300

    added AuthController

--- src/main/java/com/epam/esm/controllers/AuthController.java
+++ src/main/java/com/epam/esm/controllers/AuthController.java
@@ -1,2 +1,27 @@
-package com.epam.esm.controllers;public class AuthController {
+package com.epam.esm.controllers;
+
+import com.epam.esm.exceptions.ObjectIsExistException;
+import com.epam.esm.pojo.AuthenticationResponse;
+import com.epam.esm.pojo.RegisterRequest;
+import com.epam.esm.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) throws ObjectIsExistException {
+        return ResponseEntity.ok(authService.register(request));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> auth(@RequestBody RegisterRequest request){
+        return ResponseEntity.ok(authService.auth(request));
+    }
 }
